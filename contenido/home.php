289fe1e041f725ebd1cb543cac9ef3d099548b60
cetecom <cetecom@LCV11024>
2019-04-02 15:16:55 -0300

    primera parte 2222

---
+++ contenido/home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link href="css/estilo.css" rel="stylesheet" type="text/css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+    <div id="contenedor">
+            <div id="titulo">
+                HOSTALRIOAMAZONAS RESERVATION ONLINE
+            </div>
+            <div id="menu"><?php include('menu.php');?></div>   
+        
+        
+        
+        <p>
+            WELCOME TO HOSTAL RIO AMAZONAS
+        </p>
+
+</body>
+
---
+++ menu.php
@@ -0,0 +1,7 @@
+<div class="list-group">
+<a id="menuhome" class="list-group-item " href="<?=URLBASE;?>contenido/home.php"><i class="fas fa-home fa-2x"></i> HOME </a>
+<a id="menuabout" class="list-group-item" href="#"><i class="far fa-flag fa-2x"></i> ABOUT CHILE </a>
+<a id="menureserva" class="list-group-item" href="#"><i class="far fa-file fa-2x"></i> RESERVATION </a>
+<a id="subventas" class="list-group-item" href="#"><i class="fas fa-search fa-2x""></i> LIST RESERVATION </a>
+</div>
+
